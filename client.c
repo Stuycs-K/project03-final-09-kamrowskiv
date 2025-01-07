0cc73240eb2594dfff9f9d5924e22395452fae07
Victor <vkamrowski50@stuy.edu>
2025-01-07 14:49:28 -0500

    added stdin in client for naming players

--- client.c
+++ client.c
@@ -9,11 +9,14 @@ int main(){
     printf("Connecting to server...\n");
     //Placeholder, connection setup done here
     printf("Connected!\n");
-    
+
     struct PlayerState player;
     player.position = POS_MIDDLE;
     player.lives = MAX_LIVES;
     player.score = 0;
+    printf("What is your name? (Max 25 chars)\n");
+    fgets(player.name,50,stdin);
+
 
     printf("Player starts game with %d lives\n",player.lives);
     return 0;
